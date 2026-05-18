c9cd4a0c68e0293e64a600e609ed9fd2b61e4038
Srubka71 <vertuigames@gmail.com>
2026-05-18 19:48:52 +0200

    Update config.js

--- looper/config.js
+++ looper/config.js
@@ -205,7 +205,7 @@ const beats = [
     producer: "Unknown",
     bpm: 103,
     file: "assets/audio/beat23.mp3",
-    image: "assets/images/beat23.jepg",
+    image: "assets/images/beat23.jpeg",
     available: true
   },
        {
