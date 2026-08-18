6ce503fc6eecc139085cb41b08eb6690f13cb1f3
Srubka71 <vertuigames@gmail.com>
2026-08-18 18:48:47 +0200

    Delete crew directory

--- crew/config.js
+++
@@ -1,9 +0,0 @@
-// Konfiguracja Supabase podstrony /crew
-const SUPABASE_URL = "https://hlruehdtrwfrfagqoyve.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscnVlaGR0cndmcmZhZ3FveXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTE3ODEsImV4cCI6MjA5NDI2Nzc4MX0.W3KbmBFpkAkI7y81HfDzUyUL8n8b85i33qENiXJYLDA";
-
-let supabaseClient = null;
-
-if (window.supabase) {
-    supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-}
